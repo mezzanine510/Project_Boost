fb76328bda5523f3481852677e93065f6374b80f
Mezz <mezzanine510@gmail.com>
2019-07-31 22:57:04 -0700

    Added rocket ship

---
+++ Assets/Rocket.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProjectInput();
+    }
+
+    private void ProjectInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            print("space pressed");
+        }
+
+        if (Input.GetKey(KeyCode.A)) {
+            print("a pressed");
+        }
+    }
+}
