b13634471eee4f6482d4472c353e347f4ab8789f
Mezz <mezzanine510@gmail.com>
2019-08-05 09:47:28 -0700

    Added: obstacles, collision detection, object tags for collisions, blue and red materials, thruster sound; Modified rocket mesh

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -5,27 +5,78 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    [SerializeField] float mainThrust = 10f;
+    [SerializeField] float rcsThrust = 100f;
+
+    public Rigidbody rocketRigidbody;
+    AudioSource thrustSound;
+    public float delta;
+    bool thrustSoundIsPlaying;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rocketRigidbody = GetComponent<Rigidbody>();
+        thrustSound = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        ProjectInput();
+        delta = Time.deltaTime;
+        Thrust();
+        Rotate();
     }
 
-    private void ProjectInput()
+    void OnCollisionEnter(Collision collision)
     {
+        string tag = collision.gameObject.tag;
+        switch (tag)
+        {
+            case "Friendly":
+                print("RAWR");
+                break;
+            
+            case "Fuel":
+                print("You gawt FUEL!");
+                break;
+
+            default:
+                print("Game Over MAAAAAAAAAAN");
+                break;
+        }
+    }
+
+    private void Thrust() {
         if (Input.GetKey(KeyCode.Space))
         {
-            print("space pressed");
+            // float thrustThisFrame = mainThrust * delta;
+            rocketRigidbody.AddRelativeForce(Vector3.up * mainThrust);
+            if (!thrustSoundIsPlaying)
+            {
+                thrustSound.Play();
+                thrustSoundIsPlaying = true;
+            }
+        }
+        else
+        {
+            thrustSoundIsPlaying = false;
+            thrustSound.Stop();
+        }
+    }
+
+    private void Rotate()
+    {
+        rocketRigidbody.freezeRotation = true; // take manual control of rotation
+        float rotationThisFrame = rcsThrust * delta;
+        
+        if (Input.GetKey(KeyCode.A)) {
+            transform.Rotate(Vector3.forward * rotationThisFrame);
+        }
+        else if (Input.GetKey(KeyCode.D)) {
+            transform.Rotate(Vector3.back * rotationThisFrame);
         }
 
-        if (Input.GetKey(KeyCode.A)) {
-            print("a pressed");
-        }
+        rocketRigidbody.freezeRotation = false; // resume physics control of rotation;
     }
 }
