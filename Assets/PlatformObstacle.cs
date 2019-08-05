75172db846ec152b3ceb4247423f6bb3299cfee9
Mezz <mezzanine510@gmail.com>
2019-08-05 16:42:31 -0700

    Added Leve 2; General refactoring

---
+++ Assets/PlatformObstacle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformObstacle : MonoBehaviour
+{
+    [SerializeField] float rotationRate = 25f;
+
+    public Rigidbody platform;
+    public float delta;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        platform = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        delta = Time.deltaTime;
+        transform.Rotate(Vector3.forward * rotationRate * delta);
+    }
+}
